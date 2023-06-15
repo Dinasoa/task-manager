3c5b15574390f230ef05b81003b227c357f6d45d
dinasoa <hei.dinasoa@gmail.com>
2023-06-15 10:08:56 +0300

    chore: useLocalStorage hook

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,5 +1,15 @@
-const useLocalStorage = () => {
+import {useEffect, useState} from "react";
 
+const useLocalStorage = (key) => {
+  const [value, setValue] = useState(() => {
+    const actual = localStorage.getItem(key);
+    return actual != null ? JSON.parse(actual) : null
+  })
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [key, value]);
+
+  return [value, setValue];
 }
 
 export {
